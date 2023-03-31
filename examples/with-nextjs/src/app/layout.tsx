481d696f8a13f38c30eb8282e3d6a0c04bf9b2c1
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2023-03-31 21:34:26 +0700

    refactor: refactor CSS globals and add background color to layout body in RootLayout component

--- examples/with-nextjs/src/app/layout.tsx
+++ examples/with-nextjs/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import 'photoswipe/style.css';
+import React from 'react';
 
 export const metadata = {
   title: 'Create Next App',
@@ -9,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-neutral-900 antialiased">{children}</body>
     </html>
   );
 }
