5667552f5f6fb6ab6cf870a3c30172303b569d75
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2023-03-31 21:36:29 +0700

    style: add text color class to body element for better accessibility in RootLayout component

--- examples/with-nextjs/src/app/layout.tsx
+++ examples/with-nextjs/src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-neutral-900 antialiased">{children}</body>
+      <body className="bg-neutral-900 text-neutral-50 antialiased">{children}</body>
     </html>
   );
 }
